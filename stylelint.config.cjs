84088b8e71ee0e7ebc3a9abcead555624366f190
Rychart Redwerkz <redwerkz@noreply.codeberg.org>
2023-02-17 17:21:40 +0100

    🐛 Fix stylelint and astro files

--- stylelint.config.cjs
+++ stylelint.config.cjs
@@ -1,28 +1,35 @@
 module.exports = {
-	extends: ['stylelint-config-standard', 'stylelint-config-recommended-scss', 'stylelint-prettier/recommended'],
+	extends: ['stylelint-config-html', '@tinkoff/stylelint-config', 'stylelint-config-standard-scss'],
+	plugins: ['stylelint-scss', 'stylelint-csstree-validator'],
+	overrides: [
+		{
+			files: ['**/*.{astro,html}'],
+			customSyntax: 'postcss-html'
+		},
+		{
+			files: ['src/**/*.{scss,css}'],
+			customSyntax: 'postcss-scss'
+		}
+	],
 	rules: {
-		'color-named': 'never',
-		'font-family-name-quotes': 'always-where-required',
-		'font-weight-notation': 'named-where-possible',
-		'function-url-no-scheme-relative': true,
-		'function-url-quotes': 'always',
-		indentation: 'tab',
-		'string-quotes': 'single',
-		'value-keyword-case': 'lower',
-		'unit-disallowed-list': [],
+		'csstree/validator': {
+			syntaxExtensions: ['sass'],
+			ignoreAtrules: ['custom-at-rule', '-webkit-keyframes'],
+			ignoreProperties: ['composes', 'mask', '-webkit-mask', '-webkit'],
+			ignoreValue: '^oklch'
+		},
+		'at-rule-no-unknown': null,
+		'scss/at-rule-no-unknown': true,
+		// 'scss/selector-no-redundant-nesting-selector': true,
+
+		'scss/at-rule-no-unknown': true,
+		'number-leading-zero': 'always',
+		'color-no-hex': true,
 		'max-empty-lines': 2,
 		'no-descending-specificity': true,
 		'no-duplicate-selectors': true,
-		'font-family-no-missing-generic-family-keyword': null,
-		'property-no-unknown': [
-			true,
-			{
-				ignoreProperties: ['/^lost-/']
-			}
-		],
-		'selector-pseudo-class-no-unknown': [true, { ignorePseudoClasses: ['global'] }]
+		'selector-pseudo-class-no-unknown': [true, { ignorePseudoClasses: ['global', 'nth-last-col'] }]
 	},
 	ignoreFiles: ['node_modules/*'],
-	defaultSeverity: 'warning',
-	customSyntax: 'postcss-html'
+	defaultSeverity: 'warning'
 }
